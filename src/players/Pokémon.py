e6b2b72440e821eb91bfea68682a8015f31f2bdd
chen <chenlipy@gmail.com>
2022-01-06 16:04:44 +0200

    updated Pokémon class

--- src/players/Pokémon.py
+++ src/players/Pokémon.py
@@ -2,11 +2,13 @@ import pygame
 
 
 class Pokémon:
-    def init(self, ID, pos, value, type, hasBGrabbed):
+    def init(self, ID, pos, value, type, wasTaken, edge):
         self.pos = pos
         self.ID = ID
         self.value = value
         # type -> going up or down
         self.type = type
-        # has already been grabbed
-        self.hasBGrabbed =False
+        # has already been taken
+        self.wasTaken =False
+        self.currEdge = edge
+
